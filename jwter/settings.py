b17b6947925e25866b79aeb98bcf5d8bf1cba9d9
Ilya Skriblovsky <IlyaSkriblovsky@gmail.com>
2016-03-19 22:00:38 +0300

    running with uwsgi, dockerization fixed

--- jwter/settings.py
+++ jwter/settings.py
@@ -31,10 +31,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'jwter',                      # Or path to database file if using sqlite3.
-        'USER': 'jwter',                      # Not used with sqlite3.
-        'PASSWORD': 'jwter',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/mnt/data/jwter.sqlite',       # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'db',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -65,7 +65,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/jwter-media/'
+MEDIA_ROOT = '/mnt/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
